2a6439b7a35aeece994fa90d29532087f16ab4ba
cstapiarubio <cstapiarubio@gmail.com>
2017-11-30 00:51:48 -0300

    final

---
+++ static/js/json.js
@@ -0,0 +1,8 @@
+var a=[
+ 
+    { "id": 1, "photo": "https://static.pexels.com/photos/368740/pexels-photo-368740.jpeg", "name": "Product A", "price": 46800},
+    { "id": 2, "photo": "https://static.pexels.com/photos/40377/sandals-blue-shoes-strap-shoe-40377.jpeg", "name": "Product B", "price": 19335},
+    { "id": 3, "photo": "https://static.pexels.com/photos/292999/pexels-photo-292999.jpeg", "name": "Product C", "price": 47661},
+    { "id": 4, "photo": "https://static.pexels.com/photos/54334/running-shoe-shoe-brooks-highly-functional-54334.jpeg", "name": "Product D", "price": 24917},
+    { "id": 5, "photo": "https://static.pexels.com/photos/35191/shoes-leather-single-fashion.jpg", "name": "Product E", "price": 12680}
+];
---
+++ static/js/main.js
@@ -0,0 +1,33 @@
+/*inicio fx jquery*/
+$(document).ready(function(){
+	/*recorro con forEach mi array*/
+	a.forEach(function(el){
+		
+        var cont= $('<div/>', {'class': 'col s2 m2 l2 lista'});
+        var id=el.id;
+		var img=$('<img/>', {'src': el.photo});		
+		var name= $('<h5/>', {"text": el.name});
+		var precio=$('<p/>', {"text": el.price});
+		var button=$('<button>Add to cart</button>', {'class': 'boton'});
+		
+		/*lista de productos*/	
+	
+	cont.append(img);
+	cont.append(name);
+	cont.append(precio);
+	cont.append(button);
+	$('#contenedor').append(cont);
+	})
+
+/*al apretar botón se crea un boton "remove"  y todo el contenido (foto, nombre, precio, boton)se añade a un div vacio*/
+	$('.boton').click(function(){
+			var boton=$('<button>Remove to cart</button>', {'class': 'remove'});
+			cont.append(boton);
+			$('#mod').append(cont);	
+		/*al apretar el boton "remove" me borra los productos*/
+		$('.remove').click(function(){
+  		var parentItem= $(this).parent();
+         $(parentItem).remove();
+		});
+	});
+});
